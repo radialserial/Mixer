21c2ba6f894f59a2992c14404cfeda841eac6627
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2025-03-24 16:22:35 -0300

    chore: init

---
+++ src/main/java/com/radialserial/Mixer/MixerApplication.java
@@ -0,0 +1,13 @@
+package com.radialserial.Mixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radialserial/Mixer/MixerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radialserial.Mixer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
